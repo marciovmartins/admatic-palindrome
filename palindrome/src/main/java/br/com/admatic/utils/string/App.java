f9858158dd0c258a829ff43a0b5ae565c16a7c88
Marcio Vinicius <marciovmartins@hotmail.com>
2015-09-26 21:38:54 -0300

    - basic structure.

---
+++ palindrome/src/main/java/br/com/admatic/utils/string/App.java
@@ -0,0 +1,13 @@
+package br.com.admatic.utils.string;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
